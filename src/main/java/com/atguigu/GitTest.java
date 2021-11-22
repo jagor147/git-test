1e3244fe4c55c5d4470ba0c1f731fc8772774bc1
tom_glb <goodMorning_glb@atguigu.com>
2021-11-22 23:36:07 +0800

    First Commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author shkstart @create
+ * 2021-11-22 23:34
+ */
+public class GitTest {
+    public static void main(String args){
+        System.out.println("Git Hello");
+    }
+}
